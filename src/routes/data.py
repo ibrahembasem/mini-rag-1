1c1521a4ea1c19c90afe7372736689cbc76600a5
Ibrahem Basem <ibrahembasem03@gmail.com>
2026-03-12 02:12:23 +0300

    return uploaded file id

--- src/routes/data.py
+++ src/routes/data.py
@@ -34,7 +34,7 @@ async def upload_data(project_id: str,file: UploadFile,
     
 
     project_dir_path = ProjectController().get_project_path(project_id= project_id)
-    file_path = data_controller.generate_unique_filename(
+    file_path, file_id = data_controller.generate_unique_filepath(
         orig_file_name= file.filename,
         project_id=project_id
     )
@@ -57,7 +57,8 @@ async def upload_data(project_id: str,file: UploadFile,
 
     return JSONResponse(
             content={
-                "signal": ResponseSignal.FILE_UPOLAD_SUCCESS.value
+                "signal": ResponseSignal.FILE_UPOLAD_SUCCESS.value,
+                "file_id": file_id
             }
         )
     
