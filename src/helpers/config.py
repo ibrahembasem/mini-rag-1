6d99313d6cb071297e07e6130be97d5be44c4b12
Ibrahembasem <ibrahembasem03@gmail.com>
2026-04-03 05:21:36 +0300

    adds config for llms

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -13,6 +13,21 @@ class Setting(BaseSettings):
     MONGO_URL:  str
     MONGODB_DATABASE : str
 
+    GENERATION_BACEND : str
+    EMBEDDING_BACKEND : str  
+
+
+    OPENAI_API_KEY : str = None
+    OPENAI_API_URL : str = None
+    COHERE_API_KEY : str = None
+
+    GENERATION_MODEL_ID : str = None
+    EMBEDDING_MODEL_ID : str = None
+    EMBEDDING_SIZE : int = None
+    INPUT_DEFAULT_MAX_CHARACTERS : int = None
+    GENERATION_DEFAULT_MAX_TOKENS : int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
