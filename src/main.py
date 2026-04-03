1aacbafab6bd37d7c8552eb93404d1adf2099484
Ibrahembasem <ibrahembasem03@gmail.com>
2026-04-03 05:22:02 +0300

    add llm startup

--- src/main.py
+++ src/main.py
@@ -2,18 +2,31 @@ from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_setting
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
 
-@app.on_event("startup")
 async def startup_db_cilent():
     setting = get_setting()
     app.mongo_conn = AsyncIOMotorClient(setting.MONGO_URL)
     app.db_client = app.mongo_conn[setting.MONGODB_DATABASE]
 
-@app.on_event("shutdown")
+    llm_provider_factory = LLMProviderFactory(setting)
+
+    # generating client
+    app.generation_client = llm_provider_factory.create(provider = setting.GENERATION_BACEND)
+    app.generation_client.set_generation_model(model_id= setting.GENERATION_MODEL_ID)
+    
+    # embedding client 
+    app.embedding_client = llm_provider_factory.create(provider=setting.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=setting.EMBEDDING_MODEL_ID,embedding_size=setting.EMBEDDING_SIZE)
+
 async def shutdown_db_client():
     app.mongo_conn.close()
+
+
+app.router.lifespan.on_startup.append(startup_db_cilent)
+app.router.lifespan.on_startup.append(shutdown_db_client)
 
 
 app.include_router(base.base_router)
