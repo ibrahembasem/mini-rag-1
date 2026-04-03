072c798d86aa6bffc4d9a92cd3db880909b8a02a
Ibrahembasem <ibrahembasem03@gmail.com>
2026-04-03 05:20:31 +0300

    add llm providers and provider factory

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class LLMEnums(Enum):
 
-    OPENAI ="OPENAI"
+    OPENAI = "OPENAI"
     COHERE = "COHERE"
 
 
@@ -11,3 +11,17 @@ class OpenAIEnum(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
+
+
+class CohereEnum(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+
+class DocumentType(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
