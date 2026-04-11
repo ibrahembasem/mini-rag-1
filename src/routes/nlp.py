75a26fe7f41c3ce80c9af931d17f5f937c537117
Ibrahembasem <ibrahembasem03@gmail.com>
2026-04-11 17:31:47 +0300

    fix missed template_parser for NLPController

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -46,7 +46,8 @@ async def index_projcet(request : Request , project_id :str, push_request:PushRe
     nlp_controller = NLPController(
         vectordb_client= request.app.vectordb_client,
         generation_client = request.app.generation_client,
-        embedding_client = request.app.embedding_client
+        embedding_client = request.app.embedding_client,
+        template_parser= request.app.template_parser,
 
     )
 
@@ -112,7 +113,8 @@ async def get_project_index_info (request: Request, project_id:str):
     nlp_controller = NLPController(
         vectordb_client= request.app.vectordb_client,
         generation_client = request.app.generation_client,
-        embedding_client = request.app.embedding_client
+        embedding_client = request.app.embedding_client,
+        template_parser= request.app.template_parser
 
     )
 
