4022197a41293b786af1f376ff4c2ae2be78ea75
Github <github_user@ip-172-26-11-166.ap-south-1.compute.internal>
2026-05-16 16:47:20 +0000

    adding DIALECT FOR AI response

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -45,6 +45,9 @@ class Setting(BaseSettings):
     DEFAULT_LANG : str = "en"
     PRIMARY_LANG : str = "en"
 
+    ARABIC_DIALECT : str = None  # e.g. "كويتية", "أردنية", "مصرية"
+
+
     # WhatsApp Cloud API Settings
     WHATSAPP_VERIFY_TOKEN: str = None
     WHATSAPP_API_TOKEN: str = None
